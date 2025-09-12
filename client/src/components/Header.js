c12766c4f4a5768366ca8e174b3a95d38ccbc3e5
Nawaraj-Tamang <nawarajtamang16@gmail.com>
2025-09-12 22:49:56 +0545

    Updated Header with navbar toggler and Bootstrap JS

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,21 +19,48 @@ export default function Header() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
         <Link className="navbar-brand" to="/">LaptopBazaar</Link>
-        <div className="collapse navbar-collapse">
+
+        {/* Navbar Toggler for Mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible Navbar Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/cart">Cart ({cartCount})</Link>
             </li>
             {user ? (
               <>
-                <li className="nav-item"><span className="nav-link">Hi, {user.name}</span></li>
-                {user.isAdmin && <li className="nav-item"><Link className="nav-link" to="/admin">Admin</Link></li>}
-                <li className="nav-item"><button className="btn btn-link nav-link" onClick={logout}>Logout</button></li>
+                <li className="nav-item">
+                  <span className="nav-link">Hi, {user.name}</span>
+                </li>
+                {user.isAdmin && (
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/admin">Admin</Link>
+                  </li>
+                )}
+                <li className="nav-item">
+                  <button className="btn btn-link nav-link" onClick={logout}>Logout</button>
+                </li>
               </>
             ) : (
               <>
-                <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">Register</Link>
+                </li>
               </>
             )}
           </ul>
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { setAuthToken } from './api';
 import { CartProvider } from './CartContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const user = JSON.parse(localStorage.getItem('userInfo'));
 if (user && user.token) setAuthToken(user.token);
